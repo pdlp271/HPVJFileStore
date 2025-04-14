9c7ff4de375f578937942202a95c8fb92f1e4a2a
pdlp271 <109448588+pdlp271@users.noreply.github.com>
2025-04-14 11:48:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ API_HASH = environ.get("API_HASH", "17187c5466fb7044793bfa8bcaa9ec68")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 PICS = (environ.get('PICS', 'https://telegra.ph/file/57a9ae4c1858927eda696.jpg')).split() # Bot Start Picture
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1511468725').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "HP_FILESHAREBOT") # without @
 PORT = environ.get("PORT", "8080")
 
